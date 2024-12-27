d1fa4a8e5716efcdc8593901a9722dcfc20efb93
Kevin Hannay <khannay@umich.edu>
2024-12-26 20:22:52 -0500

    working again

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
     url = cfg['git_url'],
     packages = setuptools.find_packages(),
     include_package_data = True,
-    package_data={'': ['prcdata/*.csv']},
+    package_data={'deepPRC': ['prcdata/*.csv']},
     install_requires = requirements,
     extras_require={ 'dev': dev_requirements },
     dependency_links = cfg.get('dep_links','').split(),
